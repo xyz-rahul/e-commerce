9122cc1f750b07d9a09aa11d15ebb9b7aeccedee
Rahul Kumar <xyz.rahulkumar2002.com>
2024-03-31 23:44:28 +0530

    fix sell page ui

--- frontend/src/pages/Sell.tsx
+++ frontend/src/pages/Sell.tsx
@@ -11,7 +11,7 @@ interface Product {
 }
 
 export default function Sell() {
-    const [submitPending, setSubmitPending] = useState(false)
+    const [submitpending, setSubmitPending] = useState(false)
     const [error, setError] = useState<string | null>(null)
     const [product, setProduct] = useState<Product>({
         name: '',
@@ -66,74 +66,106 @@ export default function Sell() {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit} className="p-8 flex-col">
-                <div className="p-2">
-                    <label>
+        <div className="container mx-auto">
+            <form
+                onSubmit={handleSubmit}
+                className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+            >
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">
                         Name:
                         <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             type="text"
                             name="name"
                             value={product.name}
                             onChange={handleChange}
+                            placeholder="Enter product name"
                         />
                     </label>
                 </div>
-                <div className="p-2">
-                    <label>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">
                         Description:
                         <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             type="text"
                             name="description"
                             value={product.description}
                             onChange={handleChange}
+                            placeholder="Enter product description"
                         />
                     </label>
                 </div>
-                <div className="p-2">
-                    <label>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">
                         Price:
                         <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             type="number"
                             name="price"
                             value={product.price}
                             onChange={handleChange}
+                            placeholder="Enter product price"
                         />
                     </label>
                 </div>
-                <div className="p-2">
-                    <label>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">
                         Stock Quantity:
                         <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             type="number"
                             name="stock_quantity"
                             value={product.stock_quantity}
                             onChange={handleChange}
+                            placeholder="Enter stock quantity"
                         />
                     </label>
                 </div>
-                <div className="p-2">
-                    <label>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">
                         Category:
                         <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             type="text"
                             name="category"
                             value={product.category}
                             onChange={handleChange}
+                            placeholder="Enter product category"
                         />
                     </label>
                 </div>
-                <div className="p-2">
-                    <label>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">
                         Image:
                         <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             type="file"
                             accept=".jpg, .jpeg"
                             onChange={handleImageChange}
                         />
                     </label>
                 </div>
-                <button type="submit">Submit</button>
+                <div className="flex items-center justify-between">
+                    <button
+                        type="submit"
+                        className="inline-block rounded bg-sky-700 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white  transition duration-150 ease-in-out hover:bg-sky-500"
+                        disabled={submitpending ? true : false}
+                    >
+                        {submitpending ? (
+                            <>
+                                <div
+                                    className="inline-block h-4 w-4 mx-1 animate-spin rounded-full border-2 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                                    role="status"
+                                />
+                                <span>Loading...</span>
+                            </>
+                        ) : (
+                            <span>Submit</span>
+                        )}
+                    </button>
+                </div>
             </form>
         </div>
     )
