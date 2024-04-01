8d237c43b89f9a7928d8982ad8cad72866bcfe35
Rahul Kumar <xyz.rahulkumar2002.com>
2024-04-01 12:24:36 +0530

    updated db_schema

--- backend/db_schema.sql
+++ backend/db_schema.sql
@@ -1,4 +1,3 @@
--- Users table
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -10,7 +9,6 @@ CREATE TABLE users (
 );
 
 
--- Products table
 CREATE TABLE products (
     product_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -23,76 +21,3 @@ CREATE TABLE products (
 );
 
 
--- Inserting 2 users into the users table
-INSERT INTO
-    users (name, email, address, phone, password, role)
-VALUES
-    (
-        'John Doe',
-        'john@example.com',
-        '123 Main St, Anytown',
-        '123-456-7890',
-        'password1',
-        'user'
-    ),
-    (
-        'Jane Smith',
-        'jane@example.com',
-        '456 Elm St, Othertown',
-        '987-654-3210',
-        'password2',
-        'admin'
-    );
-
-
--- Inserting 5 products into the products table with user IDs 1 and 2
-INSERT INTO
-    products (
-        name,
-        description,
-        price,
-        stock_quantity,
-        category,
-        user_id
-    )
-VALUES
-    (
-        'Product 1',
-        'Description for Product 1',
-        19.99,
-        100,
-        'Category A',
-        1
-    ),
-    (
-        'Product 2',
-        'Description for Product 2',
-        29.99,
-        80,
-        'Category B',
-        2
-    ),
-    (
-        'Product 3',
-        'Description for Product 3',
-        39.99,
-        120,
-        'Category A',
-        1
-    ),
-    (
-        'Product 4',
-        'Description for Product 4',
-        49.99,
-        50,
-        'Category C',
-        1
-    ),
-    (
-        'Product 5',
-        'Description for Product 5',
-        59.99,
-        200,
-        'Category B',
-        2
-    );
